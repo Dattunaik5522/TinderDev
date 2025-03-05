4338b20af1e3e7d6c5a216a798fa40bfe2ed7f44
Dattunaik5252 <gugulothdattu000@gmail.com>
2025-03-05 17:01:26 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -4,42 +4,32 @@ const app = express();
 
 const PORT = 3001;
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
+app.get("/getUserData", (req, res) => {
+  //Logic for Db call to gett user data
+  // what if any error any code or db not able fetch data or any error in js code
+  // we have to hadle those cases.
+  // whenever we are writing db call we should write in try-catch blocks
 
-// app.get("/", (req, res) => {
-//Route Handler
-//   res.send("Route handler using get method only using '/' ");
-// });
-
-//
-// });
-
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req, res) => {
-  // here we don't nees authoristion to call login api
-  res.send("logged in successfully");
+  try {
+    throw new Error("sfosf");
+    res.send("User Data sent");
+  } catch (err) {
+    res.status(500).send("error encountered contact support");
+  }
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  // in this case user accessing data so we need auth here
-  res.send("User Data sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  //To check if request authorized or not
-  // const token = "xyzsfdddn";
-  // const isAdminAuthorized = token === "xyz";
-  // if (isAdminAuthorized) {
-  //   res.send("All Data sent");
-  // } else {
-  //   res.status(401).send("Unauthorized request");
-  // }
-  res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+app.use("/", (error, req, res, next) => {
+  // error should be first parameter when error handling
+  // have to follow order here
+  // if keep this at the starting the route will go through this nothing because in js code executes line by line
+  // if any error encoutered after this route execution it won't redirect to this as it executed at the start only.
+  // always keep it at the end of the code. if any error ocurs it wii execute.
+  if (error) {
+    // use the parameter we mentioned to throw errors
+    // log the error to know what errors we are gettig
+    console.log(error); //log would be: Error: sfosf
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(PORT, () => {
